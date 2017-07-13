2a41c0d84a353c6ba855e3830784a44f36891e8d
xyzz <xyzz@XYZZ>
2017-07-13 11:17:13 +0800

    第一次提交

---
+++ SearchTable/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using XYZZ.Library;
+
+namespace SearchTable
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            int index = TypeBox.SelectedIndex;
+            string text = InputText.Text;
+            bool nullable = !AllowNull.IsChecked ?? false;
+            MainContent.ItemsSource = null;
+            Loading.Visibility = Visibility.Visible;
+            Task.Run(() =>
+              {
+                  if (index == 0)
+                  {
+                      //数据表搜索
+                      return DataBaseSupport.GetLibraryFromTable(text, nullable);
+                  }
+                  else if (index == 1)
+                  {
+                      //SQL搜索
+                      return DataBaseSupport.GetLibraryFromSQL(text, nullable);
+                  }
+                  else
+                  {
+                      //同步数据表
+                      return new List<string>();
+                  }
+              }).ContinueWith((t) =>
+            {
+                if (t.Status == TaskStatus.RanToCompletion)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        t.Result.Sort();
+                        MainContent.ItemsSource = t.Result.Select(x => new { LibraryName = x });
+                        Loading.Visibility = Visibility.Hidden;
+                    });
+                }
+                else
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        Loading.Visibility = Visibility.Hidden;
+                        MessageBox.Show((t.Exception.InnerException ?? t.Exception).Message);
+                        new LogControl("ErrorLog").WriteLog(t.Exception.InnerException.ToString() ?? t.Exception.ToString());
+                    });
+                }
+            });
+        }
+    }
+}
---
+++ WpfControlLibrary/Loading.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.ComponentModel;
+
+namespace WpfControlLibrary
+{
+    public class Loading : Control
+    {
+        static Loading()
+        {
+            //重载默认样式
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Loading), new FrameworkPropertyMetadata(typeof(Loading)));
+            //DependencyProperty 注册 FillColor
+            FillColorProperty = DependencyProperty.Register("FillColor",
+            typeof(Color),
+            typeof(Loading),
+            new UIPropertyMetadata(Colors.DarkGray,
+            new PropertyChangedCallback(OnUriChanged))
+            );
+            //Colors.DarkBlue为控件初始化默认值
+
+        }
+        //属性变更回调函数
+        private static void OnUriChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #region 自定义Fields
+        // DependencyProperty属性定义 FillColorProperty=FillColor+Property组成
+        public static readonly DependencyProperty FillColorProperty;
+        #endregion
+        //VS设计器属性支持
+        [Description("背景色"), Category("个性配置"), DefaultValue("#FF668899")]
+        public Color FillColor
+        {
+            //GetValue,SetValue为固定写法，此处一般不建议处理其他逻辑
+            get { return (Color)GetValue(FillColorProperty); }
+            set { SetValue(FillColorProperty, value); }
+        }
+    }
+}
